f88df4d7f3f18bcd1a7f72631af14c115b3ee3ee
Alina bc2025-6 <ALINA.ANTOSHKOVA@lnu.edu.ua>
2026-04-15 22:41:22 +0300

    added method post and tested registration

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { Command } = require('commander');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const program = new Command();
 
@@ -22,11 +24,41 @@ if (!fs.existsSync(options.cache)) {
     console.log(`[System] Використовується існуюча директорія кешу: ${options.cache}`);
 }
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, options.cache),
+    filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
+});
+
+const upload = multer({ dest: options.cache });
+let inventory = [];
+
 const app = express();
 
-// Базовий роут, щоб перевірити, чи сервер "живий"
-app.get('/', (req, res) => {
-    res.send('Inventory Service is running!');
+app.get('/RegisterForm.html', (req, res) => {
+    res.sendFile(path.resolve('RegisterForm.html'));
+});
+
+app.get('/SearchForm.html', (req, res) => {
+    res.sendFile(path.resolve('SearchForm.html'));
+});
+
+app.post('/register', upload.single('photo'), (req, res) => {
+    const { inventory_name, description } = req.body;
+
+    // Перевірка обов'язкового поля [cite: 21, 48]
+    if (!inventory_name) {
+        return res.status(400).send('Bad Request: name is required');
+    }
+
+    const newItem = {
+        id: uuidv4(), // Генеруємо унікальний ID [cite: 16]
+        name: inventory_name,
+        description: description || '',
+        photo: req.file ? req.file.filename : null
+    };
+
+    inventory.push(newItem);
+    res.status(201).json(newItem); // Успіх - статус 201 [cite: 80]
 });
 
 // 3. Запуск сервера з параметрами host та port 
